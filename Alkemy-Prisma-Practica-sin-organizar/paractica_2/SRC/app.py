0c70d9674164e8a4217d8879751dca71c69220fc
Cristian Rosas <rosascristian26@gmail.com>
2022-11-14 07:01:15 -0300

    Comentarios en python

--- Alkemy-Prisma-Practica-sin-organizar/paractica_2/SRC/app.py
+++
@@ -1,4 +0,0 @@
-from decouple import config
-
-print('Host: {HOST} - User: {USER} - Password:{PASSWORD} - Db: {DATABASE_NAME}'.format(HOST=config('DATABASE_HOST'), USER=config('USERNAME'), PASSWORD=config('PASSWORD'), DATABASE_NAME= config('DATABASE_NAME')))
-
